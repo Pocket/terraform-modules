b81d0405794498c1409441bcc05c640405b30c37
Kelvin Yeboah <kyeboah@getpocket.com>
2020-11-06 17:27:53 -0500

    update names

--- src/base/ApplicationLambdaCodeDeploy.ts
+++ src/base/ApplicationLambdaCodeDeploy.ts
@@ -32,7 +32,7 @@ export class ApplicationLambdaCodeDeploy extends Resource {
 
   private setupCodeDeploy() {
     const codeDeployApp = new CodedeployApp(this, 'code-deploy-app', {
-      name: `${this.config.name}-LambdaCodeDeploy`,
+      name: `${this.config.name}-Lambda`,
       computePlatform: 'Lambda',
     });
 
@@ -49,7 +49,7 @@ export class ApplicationLambdaCodeDeploy extends Resource {
     new CodedeployDeploymentGroup(this, 'code-deployment-group', {
       appName: codeDeployApp.name,
       deploymentConfigName: 'CodeDeployDefault.LambdaAllAtOnce',
-      deploymentGroupName: `${this.config.name}-LambdaCodeDeployGroup`,
+      deploymentGroupName: codeDeployApp.name,
       serviceRoleArn: this.getCodeDeployRole().arn,
       deploymentStyle: [
         {
@@ -63,6 +63,7 @@ export class ApplicationLambdaCodeDeploy extends Resource {
           events: ['DEPLOYMENT_FAILURE'],
         },
       ],
+      dependsOn: [codeDeployApp],
     });
   }
 
