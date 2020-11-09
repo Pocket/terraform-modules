6662f7026e54f72aa8131ad9734467803a32b8d0
Kelvin Yeboah <kyeboah@getpocket.com>
2020-11-09 13:22:47 -0500

    remove unused import

--- src/pocket/PocketEventBridgeWithLambdaTarget.ts
+++ src/pocket/PocketEventBridgeWithLambdaTarget.ts
@@ -7,7 +7,6 @@ import {
 } from '../base/ApplicationVersionedLambda';
 import {
   DataAwsIamPolicyDocumentStatement,
-  LambdaEventSourceMapping,
   LambdaFunctionVpcConfig,
   LambdaPermission,
 } from '../../.gen/providers/aws';
