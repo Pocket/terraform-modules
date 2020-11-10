b479a892620aa88b7b1b14af6157d14ae0c5e324
Hristo Ganchev <hganchev@getpocket.com>
2020-11-10 14:42:45 -0500

    Adding tests

--- src/base/ApplicationAutoscaling.spec.ts
+++ src/base/ApplicationAutoscaling.spec.ts
@@ -3,19 +3,20 @@ import { ApplicationAutoscaling } from './ApplicationAutoscaling';
 
 test('renders autoscaling without tags', () => {
   const app = Testing.app();
-  const stack = new TerraformStack(app, 'testAutoscaling');
+  const stack = new TerraformStack(app, 'test');
 
   new ApplicationAutoscaling(stack, 'testAutoscaling', {
     prefix: 'test-',
     targetMinCapacity: 1,
-    targetMaxCapacity: 2,
+    targetMaxCapacity: 5,
     ecsClusterName: 'ecs-cluster-test',
     ecsServiceName: 'ecs-service-test',
     scalableDimension: 'ecs:service:DesiredCount',
     stepScaleOutAdjustment: 20,
     stepScaleInAdjustment: 10,
     scaleInThreshold: 1,
-    scaleOutThreshold: 2,
+    scaleOutThreshold: 3,
   });
+
   expect(Testing.synth(stack)).toMatchSnapshot();
 });
