2a8d2a4b2f03f376e8e6b2b2f79f06aee2cb5050
Mathijs Miermans <m.miermans@gmail.com>
2021-03-26 16:30:25 -0700

    Refactor container definition json building

--- src/base/ApplicationECSContainerDefinition.ts
+++ src/base/ApplicationECSContainerDefinition.ts
@@ -1,54 +1,3 @@
-export const JSON_TEMPLATE = `
-{
-  "dnsSearchDomains": null,
-  "environmentFiles": null,
-  "logConfiguration": {
-    "logDriver": "awslogs",
-    "secretOptions": [],
-    "options": {
-      "awslogs-group": [[LOG_GROUP]],
-      "awslogs-region": "us-east-1",
-      "awslogs-stream-prefix": "ecs"
-    }
-  },
-  "entryPoint": null,
-  "portMappings": [[PORT_MAPPINGS]],
-  "command": [[COMMAND]],
-  "linuxParameters": null,
-  "cpu": [[CPU]],
-  "environment": [[ENV_VARS]],
-  "resourceRequirements": null,
-  "ulimits": null,
-  "repositoryCredentials": [[REPOSITORY_CREDENTIALS_PARAMETER]],
-  "dnsServers": null,
-  "mountPoints": [],
-  "workingDirectory": null,
-  "secrets": [[SECRET_ENV_VARS]],
-  "dockerSecurityOptions": null,
-  "memory": null,
-  "memoryReservation": [[MEMORY_RESERVATION]],
-  "volumesFrom": [],
-  "stopTimeout": null,
-  "image": [[CONTAINER_IMAGE]],
-  "startTimeout": null,
-  "firelensConfiguration": null,
-  "dependsOn": null,
-  "disableNetworking": null,
-  "interactive": null,
-  "healthCheck": [[HEALTH_CHECK]],
-  "essential": true,
-  "links": null,
-  "hostname": null,
-  "extraHosts": null,
-  "pseudoTerminal": null,
-  "user": null,
-  "readonlyRootFilesystem": false,
-  "dockerLabels": null,
-  "systemControls": null,
-  "privileged": null,
-  "name": [[NAME]]
-}`;
-
 interface EnvironmentVariable {
   name: string;
   value: string;
@@ -67,19 +16,13 @@ interface HealthcheckVariable {
   timeout: number;
 }
 
-interface MountPoint {
-  containerPath: string;
-  readOnly: boolean;
-  sourceVolume: string;
-}
-
 export interface ApplicationECSContainerDefinitionProps {
   containerImage?: string;
+  // Question: logGroup is optional, but the code below will not work if it's not set. Should it be made required?
   logGroup?: string;
   hostPort?: number;
   containerPort?: number;
   envVars?: EnvironmentVariable[];
-  mountPoints?: MountPoint[];
   secretEnvVars?: SecretEnvironmentVariable[];
   command?: string[];
   name: string;
@@ -93,72 +36,66 @@ export interface ApplicationECSContainerDefinitionProps {
 export function buildDefinitionJSON(
   config: ApplicationECSContainerDefinitionProps
 ): string {
-  let templateInstance = JSON_TEMPLATE;
+  const containerDefinition = {
+    dnsSearchDomains: null,
+    environmentFiles: null,
+    logConfiguration: {
+      logDriver: 'awslogs',
+      secretOptions: [],
+      options: {
+        'awslogs-group': config.logGroup,
+        'awslogs-region': 'us-east-1',
+        'awslogs-stream-prefix': 'ecs',
+      },
+    },
+    entryPoint: null,
+    portMappings: [
+      {
+        hostPort: config.hostPort,
+        protocol: config.protocol ?? 'tcp',
+        containerPort: config.containerPort,
+      },
+    ],
+    linuxParameters: null,
+    cpu: config.cpu ?? 0,
+    environment: config.envVars ?? [],
+    resourceRequirements: null,
+    ulimits: null,
+    repositoryCredentials: config.repositoryCredentialsParam
+      ? { credentialsParameter: config.repositoryCredentialsParam }
+      : null,
+    dnsServers: null,
+    mountPoints: [],
+    workingDirectory: null,
+    secrets: config.secretEnvVars ?? null, // env vars default is [], whereas secrets default is null. makes sense.
+    dockerSecurityOptions: null,
+    memory: null,
+    memoryReservation: config.memoryReservation ?? null,
+    volumesFrom: [],
+    stopTimeout: null,
+    image: config.containerImage,
+    startTimeout: null,
+    firelensConfiguration: null,
+    dependsOn: null,
+    disableNetworking: null,
+    interactive: null,
+    healthCheck: config.healthCheck ?? null,
+    essential: true,
+    links: null,
+    hostname: null,
+    extraHosts: null,
+    pseudoTerminal: null,
+    user: null,
+    readonlyRootFilesystem: false,
+    dockerLabels: null,
+    systemControls: null,
+    privileged: null,
+    name: config.name,
+  };
 
   if (config.command) {
-    templateInstance = templateInstance.replace(
-      '[[COMMAND]]',
-      JSON.stringify(config.command)
-    );
-  } else {
-    templateInstance = templateInstance.replace('"command": [[COMMAND]],', '');
+    containerDefinition['command'] = config.command;
   }
 
-  templateInstance = templateInstance.replace(
-    '[[LOG_GROUP]]',
-    JSON.stringify(config.logGroup)
-  );
-  templateInstance = templateInstance.replace(
-    '[[HOST_PORT]]',
-    config.hostPort.toString()
-  );
-  templateInstance = templateInstance.replace(
-    '[[CONTAINER_PORT]]',
-    config.containerPort.toString()
-  );
-  templateInstance = templateInstance.replace(
-    '[[CONTAINER_IMAGE]]',
-    JSON.stringify(config.containerImage)
-  );
-  templateInstance = templateInstance.replace(
-    '[[NAME]]',
-    JSON.stringify(config.name)
-  );
-  templateInstance = templateInstance.replace(
-    '[[REPOSITORY_CREDENTIALS_PARAMETER]]',
-    JSON.stringify(
-      config.repositoryCredentialsParam
-        ? { credentialsParameter: config.repositoryCredentialsParam }
-        : null
-    )
-  );
-
-  // env vars default is [], whereas secrets default is null. makes sense.
-  templateInstance = templateInstance.replace(
-    '[[ENV_VARS]]',
-    JSON.stringify(config.envVars ?? [])
-  );
-  templateInstance = templateInstance.replace(
-    '[[SECRET_ENV_VARS]]',
-    JSON.stringify(config.secretEnvVars ?? null)
-  );
-
-  templateInstance = templateInstance.replace(
-    '[[CPU]]',
-    JSON.stringify(config.cpu ?? 0)
-  );
-
-  templateInstance = templateInstance.replace(
-    '[[MEMORY_RESERVATION]]',
-    JSON.stringify(config.memoryReservation ?? null)
-  );
-
-  templateInstance = templateInstance.replace(
-    '[[HEALTH_CHECK]]',
-    JSON.stringify(config.healthCheck ?? null)
-  );
-
-  // strip out whitespace and newlines and return
-  return templateInstance.replace(/\n/g, '');
-  //return templateInstance;
+  return JSON.stringify(containerDefinition);
 }
