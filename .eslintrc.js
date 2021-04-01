2cb0f4466ff0f2059f26d8fc886e4ed28060ec7e
Kelvin Yeboah <kyeboah@getpocket.com>
2021-04-01 09:42:47 -0400

    chore(dependencies): update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended',
   ],
   plugins: [],
