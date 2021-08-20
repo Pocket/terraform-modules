bc4c5c5f2d03f972d3ed2330302394b31dad67ba
Brett Kochendorfer <bkochendorfer@mozilla.com>
2021-08-20 11:35:52 -0500

    chore(learning) Adding some tests for RDS
    Learning about cdktf and Jest. Added some tests, this could be helpful
    or not! Feel free to close if not.

--- src/base/ApplicationRDSCluster.spec.ts
+++ src/base/ApplicationRDSCluster.spec.ts
@@ -21,3 +21,40 @@ test('renders a RDS cluster', () => {
 
   expect(Testing.synth(stack)).toMatchSnapshot();
 });
+
+test('renders RDS without tags', () => {
+  const app = Testing.app();
+  const stack = new TerraformStack(app, 'test');
+
+  new ApplicationRDSCluster(stack, 'testRDSClusterNoTags', {
+    prefix: 'bowling-',
+    vpcId: 'rug',
+    subnetIds: ['0', '1'],
+    rdsConfig: {
+      masterUsername: 'walter',
+      masterPassword: 'bowling',
+      databaseName: 'walter',
+    },
+  });
+
+  expect(Testing.synth(stack)).toMatchSnapshot();
+});
+
+test('renders RDS for mysql', () => {
+  const app = Testing.app();
+  const stack = new TerraformStack(app, 'test');
+
+  new ApplicationRDSCluster(stack, 'testRDSMysql', {
+    prefix: 'bowling-',
+    vpcId: 'rug',
+    subnetIds: ['0', '1'],
+    rdsConfig: {
+      masterUsername: 'walter',
+      masterPassword: 'bowling',
+      databaseName: 'walter',
+      engine: 'aurora-mysql',
+    },
+  });
+
+  expect(Testing.synth(stack)).toMatchSnapshot();
+});
