d456cd715c692db24c8a48f0bac12997862d4c54
Kat Schelonka <kschelonka@mozilla.com>
2021-12-02 09:38:36 -0800

    chore: revert example

--- src/example.ts
+++ src/example.ts
@@ -1,14 +1,10 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 import { AwsProvider } from '@cdktf/provider-aws';
+import { PocketALBApplication } from './pocket/PocketALBApplication';
+import { ApplicationECSContainerDefinitionProps } from './base/ApplicationECSContainerDefinition';
 import { LocalProvider } from '@cdktf/provider-local';
 import { NullProvider } from '@cdktf/provider-null';
-import {
-  PocketApiGateway,
-  PocketApiGatewayProps,
-} from './pocket/PocketApiGatewayLambdaIntegration';
-import { LAMBDA_RUNTIMES } from './base/ApplicationVersionedLambda';
-import { ArchiveProvider } from '@cdktf/provider-archive';
 
 class Example extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -19,31 +15,48 @@ class Example extends TerraformStack {
     });
     new LocalProvider(this, 'local', {});
     new NullProvider(this, 'null', {});
-    new ArchiveProvider(this, 'archive', {});
 
-    const config: PocketApiGatewayProps = {
-      name: 'test-api-lambda',
-      stage: 'test',
-      domain: 'exampleapi.getpocket.dev',
-      routes: [
+    const containerConfigBlue: ApplicationECSContainerDefinitionProps = {
+      name: 'blueContainer',
+      containerImage: 'bitnami/node-example:0.0.1',
+      portMappings: [
         {
-          path: 'endpoint',
-          method: 'POST',
-          eventHandler: {
-            name: 'lambda-endpoint',
-            lambda: {
-              runtime: LAMBDA_RUNTIMES.PYTHON38,
-              handler: 'index.handler',
-            },
-          },
+          hostPort: 3000,
+          containerPort: 3000,
+        },
+      ],
+      envVars: [
+        {
+          name: 'foo',
+          value: 'bar',
         },
       ],
     };
 
-    new PocketApiGateway(this, 'example', config);
+    new PocketALBApplication(this, 'example', {
+      exposedContainer: {
+        name: 'blueContainer',
+        port: 3000,
+        healthCheckPath: '/',
+      },
+      codeDeploy: {
+        useCodeDeploy: true,
+      },
+      cdn: false, // maybe make this false if you're testing an actual terraform apply - cdn's take a loooong time to spin up
+      alb6CharacterPrefix: 'ACMECO',
+      internal: false,
+      domain: 'acme.getpocket.dev',
+      prefix: 'ACME-Dev', // Prefix is a combo of the `Name-Environment`
+      containerConfigs: [containerConfigBlue],
+      ecsIamConfig: {
+        prefix: 'ACME-Dev',
+        taskExecutionRolePolicyStatements: [],
+        taskRolePolicyStatements: [],
+      },
+    });
   }
 }
 
 const app = new App();
-new Example(app, 'apig-test');
+new Example(app, 'acme-example');
 app.synth();
