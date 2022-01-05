5a340ad0f999f0896a6bf9d6eb26042869c33427
Mathijs Miermans <m.miermans@gmail.com>
2022-01-05 14:28:56 -0800

    test(pocket-ecs-codepipeline): add a test for custom codepipeline actions

--- src/pocket/PocketECSCodePipeline.spec.ts
+++ src/pocket/PocketECSCodePipeline.spec.ts
@@ -3,6 +3,7 @@ import {
   PocketECSCodePipeline,
   PocketECSCodePipelineProps,
 } from './PocketECSCodePipeline';
+import { codepipeline } from '@cdktf/provider-aws';
 
 const config: PocketECSCodePipelineProps = {
   prefix: 'Test-Env',
@@ -84,6 +85,43 @@ test('renders a Pocket ECS Codepipeline template with the provided taskdef path'
         taskDefPath: 'testtaskdef.json',
       },
     });
+  });
+  expect(synthed).toMatchSnapshot();
+});
+
+test('renders a Pocket ECS Codepipeline template with a custom step', () => {
+  class CustomECSCodePipeline extends PocketECSCodePipeline {
+    protected getDeployStage = (): codepipeline.CodepipelineStage => {
+      const deployStage = super.getDeployStage();
+      const maxRunOrder = Math.max(
+        ...deployStage.action.map((action) => action.runOrder),
+        1 // default runOrder if deployStage is empty.
+      );
+
+      // Add a custom action to the stage with a higher runOrder, to run it at the end.
+      deployStage.action.push(this.getCustomDeployAction(maxRunOrder + 1));
+
+      return deployStage;
+    };
+
+    protected getCustomDeployAction = (
+      runOrder: number
+    ): codepipeline.CodepipelineStageAction => ({
+      name: 'Deploy_Prefect_Cloud',
+      category: 'Deploy',
+      owner: 'AWS',
+      provider: 'StepFunctions',
+      version: '1',
+      configuration: {
+        stateMachineArn: 'myStateMachineArn123',
+        ExecutionNamePrefix: 'CodePipelineDeploy',
+      },
+      runOrder: runOrder,
+    });
+  }
+
+  const synthed = Testing.synthScope((stack) => {
+    new CustomECSCodePipeline(stack, 'test-codepipeline', config);
   });
   expect(synthed).toMatchSnapshot();
 });
