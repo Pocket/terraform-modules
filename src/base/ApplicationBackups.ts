454631202708c69ab02c7c637743889909450cc1
Hristo Ganchev <hganchev@getpocket.com>
2022-03-21 15:30:12 -0400

    Linting update

--- src/base/ApplicationBackups.ts
+++ src/base/ApplicationBackups.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import { Resource } from 'cdktf';
-import { Backup, IAM } from '@cdktf/provider-aws';
+import { Backup } from '@cdktf/provider-aws';
 import BackupVault = Backup.BackupVault;
 import BackupVaultPolicy = Backup.BackupVaultPolicy;
 import BackupPlan = Backup.BackupPlan;
@@ -8,65 +8,64 @@ import BackupSelection = Backup.BackupSelection;
 import BackupPlanRule = Backup.BackupPlanRule;
 
 export interface ApplicationBackupProps {
+  name: string;
+  kmsKeyArn: string;
+  prefix: string;
+  accountId: string;
+  vaultPolicy: string;
+  backupPlans: {
     name: string;
-    kmsKeyArn: string;
-    prefix: string;
-    accountId: string;
-    vaultPolicy: string;
-    backupPlans: {
-      name: string;
-      resources: string[];
-      rules: Omit<BackupPlanRule, 'targetVaultName'>[];
-      // selectionTag?: Omit<BackupPlanRule, 'targetVaultName'>[];
-      selectionTag: any
-    }[];
-    tags?: { [key: string]: string };
+    resources: string[];
+    rules: Omit<BackupPlanRule, 'targetVaultName'>[];
+    // selectionTag?: Omit<BackupPlanRule, 'targetVaultName'>[];
+    selectionTag: any
+  }[];
+  tags?: { [key: string]: string };
+}
+
+export class ApplicationBackup extends Resource {
+
+  public backupPlan: Backup.BackupPlan;
+  public backupSelection: Backup.BackupSelection;
+  public backupPlanRule: Backup.BackupPlanRule;
+  private static vault: Backup.BackupVault;
+
+
+  constructor(
+    scope: Construct,
+    name: string,
+    private config: ApplicationBackupProps) {
+    super(scope, name);
+
+    const vault = new BackupVault(this, 'backup-vault', {
+      name: `${config.prefix}-${config.name}`,
+      kmsKeyArn: config.kmsKeyArn,
+      tags: config.tags,
+    })
+
+    const vaultPolicy = new BackupVaultPolicy(this, 'backup-vault-policy', {
+      backupVaultName: `${config.prefix}-${config.name}`,
+      policy: config.vaultPolicy
+    })
+      ;
+
+    config.backupPlans.forEach((plan) => {
+      const backupPlan = new BackupPlan(this, 'backup-plan', {
+        name: plan.name,
+        rule: plan.rules.map((rule) => ({
+          ...rule,
+          targetVaultName: vault.name,
+        })),
+        tags: config.tags,
+      });
+
+      new BackupSelection(this, 'backup-selection', {
+        name: `${config.prefix}-Backup-Selection`,
+        planId: backupPlan.id,
+        iamRoleArn: `arn:aws:iam::${config.accountId}:role/service-role/AWSBackupDefaultServiceRole`,
+        resources: plan.resources,
+        selectionTag: plan.selectionTag
+      });
+    });
   }
-
-export class ApplicationBackup extends Resource {    
-
-    public backupPlan: Backup.BackupPlan;
-    public backupSelection: Backup.BackupSelection;
-    public backupPlanRule: Backup.BackupPlanRule;
-    private static vault: Backup.BackupVault;
-
-
-    constructor(
-      scope: Construct, 
-      name: string, 
-      private config: ApplicationBackupProps) 
-      {
-        super(scope, name);  
-              
-        const vault = new BackupVault(this, 'backup-vault', {
-            name: `${config.prefix}-${config.name}`,
-            kmsKeyArn: config.kmsKeyArn,
-            tags: config.tags,
-        })
-        
-        const vaultPolicy = new BackupVaultPolicy(this, 'backup-vault-policy', {
-            backupVaultName: `${config.prefix}-${config.name}`,
-            policy: config.vaultPolicy
-        })
-        ;
-
-        config.backupPlans.forEach((plan) => {
-            const backupPlan = new BackupPlan(this, 'backup-plan', {
-              name: plan.name,
-              rule: plan.rules.map((rule) => ({
-                ...rule,
-                targetVaultName: vault.name,
-              })),
-              tags: config.tags,
-            });
-
-            new BackupSelection(this, 'backup-selection', {
-                name: `${config.prefix}-Backup-Selection`,
-                planId: backupPlan.id,
-                iamRoleArn: `arn:aws:iam::${config.accountId}:role/service-role/AWSBackupDefaultServiceRole`,
-                resources: plan.resources,
-                selectionTag: plan.selectionTag
-              });
-            });
-          }
 }
