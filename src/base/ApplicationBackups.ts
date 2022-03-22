a0e65f67ef7499763f2dd0ba556476575b5b4dd6
Hristo Ganchev <hganchev@getpocket.com>
2022-03-22 14:04:13 -0400

    update

--- src/base/ApplicationBackups.ts
+++ src/base/ApplicationBackups.ts
@@ -1,11 +1,11 @@
 import { Construct } from 'constructs';
 import { Resource } from 'cdktf';
-import { Backup } from '@cdktf/provider-aws';
-import BackupVault = Backup.BackupVault;
-import BackupVaultPolicy = Backup.BackupVaultPolicy;
-import BackupPlan = Backup.BackupPlan;
-import BackupSelection = Backup.BackupSelection;
-import BackupPlanRule = Backup.BackupPlanRule;
+import {backup } from '@cdktf/provider-aws';
+import BackupVault = backup.BackupVault;
+import BackupVaultPolicy = backup.BackupVaultPolicy;
+import BackupPlan = backup.BackupPlan;
+import BackupSelection = backup.BackupSelection;
+import BackupPlanRule = backup.BackupPlanRule;
 
 export interface ApplicationBackupProps {
   name: string;
@@ -17,16 +17,16 @@ export interface ApplicationBackupProps {
     name: string;
     resources: string[];
     rules: Omit<BackupPlanRule, 'targetVaultName'>[];
-    selectionTag: Backup.BackupSelectionSelectionTag[];
+    selectionTag: backup.BackupSelectionSelectionTag[];
   }[];
   tags?: { [key: string]: string };
 }
 
 export class ApplicationBackup extends Resource {
-  public backupPlan: Backup.BackupPlan;
-  public backupSelection: Backup.BackupSelection;
-  public backupPlanRule: Backup.BackupPlanRule;
-  private static vault: Backup.BackupVault;
+  public backupPlan: backup.BackupPlan;
+  public backupSelection: backup.BackupSelection;
+  public backupPlanRule: backup.BackupPlanRule;
+  private static vault: backup.BackupVault;
 
   constructor(
     scope: Construct,
