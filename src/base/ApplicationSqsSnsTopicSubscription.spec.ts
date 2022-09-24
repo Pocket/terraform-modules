e291811467ec286ee0b50edd9698b55e90a3aade
Sri Kirsh <skirsh@mozilla.com>
2022-09-23 17:03:27 -0700

    feat:  DLQ as an optional parameter to ApplicationSqsSnsTopicSubscriptionProps (#1005)

--- src/base/ApplicationSqsSnsTopicSubscription.spec.ts
+++ src/base/ApplicationSqsSnsTopicSubscription.spec.ts
@@ -11,6 +11,17 @@ describe('ApplicationSqsSnsTopicSubscription', () => {
     }),
   });
 
+  const getConfigWithDlq = (stack) => ({
+    name: 'test-sns-subscription',
+    snsTopicArn: 'arn:aws:sns:TopicName',
+    sqsQueue: new sqs.SqsQueue(stack, 'sqs', {
+      name: 'test-sqs',
+    }),
+    snsDlq: new sqs.SqsQueue(stack, 'dlq', {
+      name: 'test-sqs-dlq',
+    }),
+  });
+
   it('renders an SQS SNS subscription without tags', () => {
     const synthed = Testing.synthScope((stack) => {
       new ApplicationSqsSnsTopicSubscription(
@@ -26,6 +37,16 @@ describe('ApplicationSqsSnsTopicSubscription', () => {
     const synthed = Testing.synthScope((stack) => {
       new ApplicationSqsSnsTopicSubscription(stack, 'sqs-sns-subscription', {
         ...getConfig(stack),
+        tags: { hello: 'there' },
+      });
+    });
+    expect(synthed).toMatchSnapshot();
+  });
+
+  it('renders an SQS SNS subscription witg dlq passed', () => {
+    const synthed = Testing.synthScope((stack) => {
+      new ApplicationSqsSnsTopicSubscription(stack, 'sqs-sns-subscription', {
+        ...getConfigWithDlq(stack),
         tags: { hello: 'there' },
       });
     });
